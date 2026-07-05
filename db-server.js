994ec10cbafd0946bd224149cd76156882bf6fd9
vannesalopez1994-sys <vannesalopez1994@gmail.com>
2026-07-05 15:25:34 -0400

    Ajuste de puerto dinamico para Render

--- db-server.js
+++ db-server.js
@@ -6,7 +6,7 @@ import pg from 'pg';
 import nodemailer from 'nodemailer';
 const { Pool } = pg;
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Cargar variables de entorno desde el archivo .env manualmente
 try {
